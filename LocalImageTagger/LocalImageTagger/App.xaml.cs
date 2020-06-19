6c5f6dba9dffbeadc55b080cbf034f180097e265
Colin Brooks <brookstco@gmail.com>
2020-06-19 15:22:06 -0400

    Minor additions
    I'm finally back, and have worked through more documentation

--- LocalImageTagger/LocalImageTagger/App.xaml.cs
+++ LocalImageTagger/LocalImageTagger/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,23 @@ namespace LocalImageTagger
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ImageViewer window = new ImageViewer();
+            if (e.Args.Length >= 1)
+            {
+                //This will enable searching and other effects from the command line by adding the search terms as parameters on bootup
+                //Open immediately in new tab if ones are saved from before?
+            }
+            /*Globalization controls. Unneeded for now.
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US"); //Sets thread default culture- Use if I multithread
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US"); //Sets number display culture
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US"); //Sets language deafult
+            */
+
+            window.Show();
+        }
     }
 }
