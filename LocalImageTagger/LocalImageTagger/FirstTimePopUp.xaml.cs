cb8bd1eb109054b710fd9776ca266e846cefd516
Colin Brooks <brookstco@gmail.com>
2020-06-19 16:01:10 -0400

    Popup and language
    Added a popup window and the basic of globilization. Neither are fully implemented, but the framework is getting there. Globilization won't be needed until it is woking though

---
+++ LocalImageTagger/LocalImageTagger/FirstTimePopUp.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LocalImageTagger
+{
+    /// <summary>
+    /// Interaction logic for FirstTimePopUp.xaml
+    /// </summary>
+    public partial class FirstTimePopUp : Window
+    {
+        public FirstTimePopUp()
+        {
+            InitializeComponent();
+        }
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+    }
+}
