6d4dc523e03313a62bb10977ec3e0b7666a05547
Colin Brooks <brookstco@gmail.com>
2020-07-03 11:37:04 -0400

    Summaries

--- LocalImageTagger/LocalImageTagger/Enums/TagType.cs
+++ LocalImageTagger/LocalImageTagger/Enums/TagType.cs
@@ -4,10 +4,22 @@ using System.Text;
 
 namespace LocalImageTagger.Enums
 {
+    /// <summary>
+    /// The type  of tag. Changes how the "TagAssociations" field is used.
+    /// </summary>
     public enum TagType : short
     {
+        /// <summary>
+        /// The enum was created erroneously.
+        /// </summary>
         Unknown,
+        /// <summary>
+        /// Normal tag behavior. The "TagAssociations" field contains the tag's parents.
+        /// </summary>
         Standard,
+        /// <summary>
+        /// The tag is an Alias. The "TagAssociations" field contains all of the standard tags that this alias is for.
+        /// </summary>
         Alias
     }
 }
