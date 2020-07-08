e88ba1c3328a5e3270fd318657c23c549fde09e1
Colin Brooks <brookstco@gmail.com>
2020-07-08 18:34:44 -0400

    Converter
    path to image converter. May be better to change it to ItemToDisplay converter, that also decides on which form is displayed

---
+++ LocalImageTagger/LocalImageTagger/Converter/PathToImageConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace LocalImageTagger
+{
+    /// <summary>
+    /// Converts a full path of an image to its bitmap
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(BitmapImage))] //Makes this findable from bindings
+    public class PathToImageConverter : IValueConverter
+    {
+        public static PathToImageConverter instance = new PathToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Get the full path if not null
+            if(value == null)
+            {
+                return null;
+            }
+            var path = (string)value;
+            return new BitmapImage(new Uri(path));
+        }
+
+        //Not implemented
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
