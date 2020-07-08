4a00373fdc4a720a62107b881d420451f8b53548
Colin Brooks <brookstco@gmail.com>
2020-07-08 19:37:17 -0400

    Better Converter
    Added converters for the item to images. Loads an unknown file image if it is not an image. I made the image, but it is a bit small and mediocre

---
+++ LocalImageTagger/LocalImageTagger/Converter/ItemToDisplayConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace LocalImageTagger
+{
+    /// <summary>
+    /// Returns the appropriate display item for the given item
+    /// </summary>
+    class ItemToDisplayConverter
+    {
+        //TODO: This is the same as ItemToImageConverter. Fix this or delete
+        public static ItemToDisplayConverter instance = new ItemToDisplayConverter();
+        /// <summary>
+        /// Currently only displays the image if valid, and an unknown otherwise. IT is actually an ItemToImageConverter right now
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //If there is no given object
+            if (value == null)
+            {
+                return null;
+            }
+            //else
+            var item = (FileItem)value;
+
+            //Returns 
+            if(item.Type == FileCategoryType.Image)
+            {
+                return new BitmapImage(new Uri(item.FullPath));
+            }
+            else
+            {
+                return new BitmapImage(new Uri("Images/unknownFile.png"));
+            }
+        }
+
+        //Not implemented
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LocalImageTagger/LocalImageTagger/Converter/ItemToImageConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace LocalImageTagger
+{
+    /// <summary>
+    /// Returns the appropriate display item for the given item
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(BitmapImage))] //Makes this findable from bindings
+    class ItemToImageConverter
+    {
+        public static ItemToDisplayConverter instance = new ItemToDisplayConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //If there is no given object
+            if (value == null)
+            {
+                return null;
+            }
+            //else
+            var item = (FileItem)value;
+
+            //Returns 
+            if(item.Type == FileCategoryType.Image)
+            {
+                return new BitmapImage(new Uri(item.FullPath));
+            }
+            else
+            {
+                return new BitmapImage(new Uri("Images/unknownFile.png"));
+            }
+        }
+
+        //Not implemented
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
