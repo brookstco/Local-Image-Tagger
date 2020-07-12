674158b2feba6203b1ec936ad1799cf1afeb8110
Colin Brooks <brookstco@gmail.com>
2020-07-12 13:34:39 -0400

    Category changes
    Categories should be counted as tags with some modifications to allow searches through a single list (hopefully that is actually a good idea). More outlines of functions to account for that, and cleaned the file up somewhat

--- LocalImageTagger/LocalImageTagger/Database/DatabaseAccess.cs
+++ LocalImageTagger/LocalImageTagger/Database/DatabaseAccess.cs
@@ -6,18 +6,30 @@ namespace LocalImageTagger
 {
     class DatabaseAccess
     {
+
         const int nameLength = 100;
         const int uriLength = 2000;
-        //others for the tags
+        //+ others for the tags
+
+
+        #region Searching 
 
         /// <summary>
-        /// Returns a list of all of the tags (and their counts) that begin with the letters that are currently being typed. Updated on each keyboard change in the search bar.
+        /// Returns a list of all of the tags (and their counts) that begin with the letters that are currently being typed, restricted by category. 
+        /// Updated on each keyboard change in the search bar.
         /// </summary>
         /// <param name="search"></param>
-        public void suggestTags(string search)
+        /// <param name="categoryID">The ID of the category the tag belongs to</param>
+        public void suggestTags(string search, int categoryID)
         {
-            //Returns a list suitable for a dropdown of all of the tags
+            //If the tag category is null, it is a category, and should be given as an option. Otherwise, restrict by the provided category
+
+            //Returns a list (LINQ, probably) suitable for a dropdown of all of the tags
+            //prioritize categories first (and make it clear that they're categories
+            //Then order by the count
         }
+
+        //TODO: Once a tag is selected from this list (or they type in a complete match and press space), add its id into the List. Caategories must be followed by a tag, and only the following tag gets added.
 
         /// <summary>
         /// When an alias has been entered, it is replaced by its attached normal tags if they aren't already in the search
@@ -59,30 +71,60 @@ namespace LocalImageTagger
         /// The actual search for tags. Calls the final preprocessing to account for aliases and parents, and returns a list of the files
         /// </summary>
         /// <param name="searchIDs"></param>
-        public void searchDatabase(List<int> searchIDs)
+        public void searchDatabaseByTags(List<int> searchIDs)
         {
             searchIDs = preprocessTags(searchIDs);
             //search DB using the list of IDs. Directly search the tagmap table, and use the file IDS to find the data in the file table. Return a sortable list of results
         }
+
+        #endregion
+
+        #region Add into database
 
         /// <summary>
         /// Adds a new tag into the database
         /// </summary>
         /// <param name="name">The name of the new tag</param>
         /// <param name="categoryID">The category that this tag belongs to</param>
-        /// <param name="tagType">The Closest value ot a tinyint, which is what this is stored as</param>
         /// <param name="aliases">The alias IDs already in a comma seperated string</param>
         /// <param name="parents">The parent IDs already in a comma seperated string</param>
         /// <param name="children">The children IDs already in a comma seperated string</param>
         /// <param name="description">The desciption of the tag.</param>
-        public void addTag(string name, int categoryID, byte tagType, string aliases, string parents, string children, string description)
+        public void addTag(string name, int categoryID, string aliases, string parents, string children, string description)
         {
+            var tagType = TagType.Standard;
             //Add current date
             //check that all the fields are good
             //add new tag to DB
         }
 
         //TODO: Updaters for each value for all tables should be made for editing entries.
+
+        public void addAlias(string name, string aliases)
+        {
+            var tagType = TagType.Alias;
+            //add into tag database as an alias
+        }
+
+        /// <summary>
+        /// Adds a new category into the tag database
+        /// </summary>
+        /// <param name="name">The name of the new category</param>
+        /// <param name="description">The desciption of the category</param>
+        public void addCategory(string name, string description)
+        {
+            //After ensuring it will actually fit, add it to the DB
+            if (stringSizeFitsUnicode(name, nameLength))
+            {
+                var tagType = TagType.Category;
+                //Add into database as new category type tag
+            }
+            else
+            {
+                //TODO: Custom exception with handleing, or just deal with this elsewhere
+                throw new System.Exception();
+            }
+        }
 
         /// <summary>
         /// Adds a new file into the database
@@ -102,36 +144,11 @@ namespace LocalImageTagger
             }
         }
 
-        /// <summary>
-        /// Adds a new category into the database
-        /// </summary>
-        /// <param name="name">The name of the new category</param>
-        public void addCategory(string name)
-        {
-            //After ensuring it will actually fit, add it to the DB
-            if (stringSizeFitsUnicode(name, nameLength))
-            {
-                //Add into database as new category
-            }
-            else
-            {
-                //TODO: Custom exception with handleing, or just deal with this elsewhere
-                throw new System.Exception();
-            }
-        }
+        #endregion
 
-        /// <summary>
-        /// Returns whether the byte size of the string is less than the wanted size.
-        /// Uses Unicode encoding
-        /// </summary>
-        /// <param name="entry">The string to be measured</param>
-        /// <param name="size">The maximum size in bytes</param>
-        /// <returns></returns>
-        public bool stringSizeFitsUnicode(string entry, int size)
-        {
-            //Comparision returns a bool value for the byte count of the string
-            return (Encoding.Unicode.GetByteCount(entry) <= size);
-        }
+
+
+        #region Tagmap
 
         /// <summary>
         /// Adds tags to an image
@@ -165,5 +182,33 @@ namespace LocalImageTagger
         {
             //remove the entry that matches
         }
+
+        #endregion
+
+        #region Helpers
+        /// <summary>
+        /// Returns whether the byte size of the string is less than the wanted size.
+        /// Uses Unicode encoding
+        /// </summary>
+        /// <param name="entry">The string to be measured</param>
+        /// <param name="size">The maximum size in bytes</param>
+        /// <returns></returns>
+        public bool stringSizeFitsUnicode(string entry, int size)
+        {
+            //Comparision returns a bool value for the byte count of the string
+            return (Encoding.Unicode.GetByteCount(entry) <= size);
+        }
+
+        /// <summary>
+        /// Converts the strings that store the IDs of alias, parents, and children into a list of ints
+        /// </summary>
+        /// <param name="IDString"></param>
+        /// <returns></returns>
+        public List<int> convertToIDList(string IDString)
+        {
+            //SQL STRING_SPLIT
+            return null;
+        }
+        #endregion
     }
 }
--- LocalImageTagger/LocalImageTagger/Enums/TagType.cs
+++ LocalImageTagger/LocalImageTagger/Enums/TagType.cs
@@ -1,21 +1,25 @@
 ﻿namespace LocalImageTagger
 {
     /// <summary>
-    /// The type  of tag. Changes how the "TagAssociations" field is used.
+    /// The type  of tag. Changes how the "Alias" field is used.
     /// </summary>
-    public enum TagType : short
+    public enum TagType : byte
     {
         /// <summary>
         /// The enum was created erroneously.
         /// </summary>
         Unknown,
         /// <summary>
-        /// Normal tag behavior. The "TagAssociations" field contains the tag's parents.
+        /// Normal tag behavior. The "Alias" field contains all of the aliases for this tag
         /// </summary>
         Standard,
         /// <summary>
-        /// The tag is an Alias. The "TagAssociations" field contains all of the standard tags that this alias is for.
+        /// The tag is an Alias. The "Alias" field contains all of the standard tags that this alias is for.
         /// </summary>
-        Alias
+        Alias,
+        /// <summary>
+        /// The tag is a category. It will behave completely differently
+        /// </summary>
+        Category
     }
 }
