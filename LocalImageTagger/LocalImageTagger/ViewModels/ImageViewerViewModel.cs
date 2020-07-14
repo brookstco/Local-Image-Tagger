29c90a255162ed5dc22941b816c0f22bd76bd9a0
Colin Brooks <brookstco@gmail.com>
2020-07-14 12:43:11 -0400

    ImageViewer Refinment
    Decided to split up the image and video viewer and not deal with the usercontrols. Added a nuget package for zooming and panning images

--- LocalImageTagger/LocalImageTagger/ViewModels/ImageViewerViewModel.cs
+++ LocalImageTagger/LocalImageTagger/ViewModels/ImageViewerViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.IO;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace LocalImageTagger.ViewModels
 {
@@ -7,16 +10,51 @@ namespace LocalImageTagger.ViewModels
         /// <summary>
         /// The viewmodel to display the appropraite file type
         /// </summary>
-        public BaseViewModel SelectedDisplayType { get; set; }
+        //public BaseViewModel SelectedDisplayType { get; set; }
 
+        /// <summary>
+        /// Command to swap the stretch property for the window
+        /// </summary>
         public ICommand SwapSize { get; set; }
 
+        /// <summary>
+        /// The current Stretch property for the image
+        /// Set as an enum: 0 = None, 2 = Uniform
+        /// </summary>
+        public int ImageStretch { get; set; }
+
+        //TODO:  May be better to use a converter to allow zoom
+        /// <summary>
+        /// The Bitmap image form of the image.
+        /// </summary>
+        public BitmapImage Image { get; set; }
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public ImageViewerViewModel()
         {
-            SelectedDisplayType = new ImageDisplayViewModel();
-            SwapSize = new RelayCommand(((ImageDisplayViewModel)SelectedDisplayType).swapSize);
+            //SelectedDisplayType = new ImageDisplayViewModel();
+            SwapSize = new RelayCommand(swapSize);
+            Image = new BitmapImage(new Uri(Path.Join("C:", "Users", "Colin", "Pictures", "cat.png")));
+            ImageStretch = 0;
         }
 
+        /// <summary>
+        /// Swaps the image's stretch property between Uniform and None
+        /// </summary>
+        public void swapSize()
+        {
+            if (ImageStretch == 0)
+            {
+                ImageStretch = 2; //Uniform
+            }
+            else
+            {
+                ImageStretch = 0; //None
+            }
+        }
 
     }
 }
---
+++ LocalImageTagger/LocalImageTagger/Views/VideoViewer.xaml.cs
@@ -0,0 +1,17 @@
+﻿using LocalImageTagger.ViewModels;
+using System.Windows;
+
+namespace LocalImageTagger.Views
+{
+    /// <summary>
+    /// Interaction logic for VideoViewer.xaml
+    /// </summary>
+    public partial class VideoViewer : Window
+    {
+        public VideoViewer()
+        {
+            InitializeComponent();
+            DataContext = new VideoViewerViewModel();
+        }
+    }
+}
