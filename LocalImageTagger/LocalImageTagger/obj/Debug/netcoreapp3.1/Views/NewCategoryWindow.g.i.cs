f9d35c745239f4a7131aa32aa5792af8c12a44f1
Colin Brooks <brookstco@gmail.com>
2020-07-22 17:22:31 -0400

    Tab swap
    Changing tab system. removed nuget and code. Started adding new system

--- LocalImageTagger/LocalImageTagger/obj/Debug/netcoreapp3.1/Views/NewCategoryWindow.g.i.cs
+++ LocalImageTagger/LocalImageTagger/obj/Debug/netcoreapp3.1/Views/NewCategoryWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace LocalImageTagger.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LocalImageTagger;component/views/newcategorywindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LocalImageTagger;V1.0.0.0;component/views/newcategorywindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\NewCategoryWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
