af5a2fdd5a482c70bab917e2046dceb729f0444d
Colin Brooks <brookstco@gmail.com>
2020-07-25 14:20:57 -0400

    Searchtab
    Better layout. Page for results butons and better item layout

--- LocalImageTagger/LocalImageTagger/ViewModels/Tabs/SearchTabViewModel.cs
+++ LocalImageTagger/LocalImageTagger/ViewModels/Tabs/SearchTabViewModel.cs
@@ -1,8 +1,12 @@
 ﻿
+using System.Collections.ObjectModel;
+
 namespace LocalImageTagger.ViewModels
 {
     public class SearchTabViewModel : BaseTabViewModel
     {
+
+        public ObservableCollection<FileItem> FileItems { get; set; }
 
         public SearchTabViewModel()
         {
