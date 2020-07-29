71fa717acba4e97947f9ba0604f6b9699e9cabd4
Colin Brooks <brookstco@gmail.com>
2020-07-29 16:24:55 -0400

    Upgrade Relay command
    Upgrading the relay command from the super basic one that I had used

--- LocalImageTagger/LocalImageTagger/ViewModels/Base/RelayCommand.cs
+++ LocalImageTagger/LocalImageTagger/ViewModels/Base/RelayCommand.cs
@@ -4,56 +4,90 @@ using System.Windows.Navigation;
 
 namespace LocalImageTagger
 {
+    // Modified from various stackoverflow posts
+
     /// <summary>
-    /// Basic command that runs an Action
+    /// A command that runs an Action.
     /// </summary>
-    public class RelayCommand : ICommand
+    public class RelayCommand<T> : ICommand
     {
-        #region Private Members
+        #region Fields
 
         /// <summary>
-        /// The action to run
+        /// The action to run.
         /// </summary>
-        private Action action;
+        private readonly Action<T> execute = null;
+
+        /// <summary>
+        /// The condition for running.
+        /// </summary>
+        private readonly Func<T, bool> canExecute;
+        //Was originally a predicate, but MDSN recommends func instead
+        //readonly Predicate<T> canExecute = null;
 
         #endregion
 
-        #region Public Events
+        #region Constructors
 
         /// <summary>
-        /// Fired when <see cref="CanExecute(object)"/> value has changed
+        /// Initializes a new instance of <see cref="DelegateCommand{T}"/>.
         /// </summary>
-        public event EventHandler CanExecuteChanged = (sender, e) => {};
-
-        #endregion
-
-        #region Constructor
-
-        public RelayCommand(Action relayedAction)
+        /// <param name="execute">Delegate to execute when Execute is called on the command.  This can be null to just hook up a CanExecute delegate.</param>
+        /// <remarks><seealso cref="CanExecute"/> will always return true.</remarks>
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
         {
-            this.action = relayedAction;
         }
 
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute) //public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
 
         #endregion
 
-        #region Commands
+        #region ICommand Members
 
-        /// <summary>
-        /// Relay Commands can always execute
-        /// </summary>
-        /// <returns>True</returns>
+        ///<summary>
+        ///Defines the method that determines whether the command can execute in its current state.
+        ///</summary>
+        ///<param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        ///<returns>
+        ///true if this command can be executed; otherwise, false.
+        ///</returns>
         public bool CanExecute(object parameter)
         {
-            return true;
+            //Simplified, but kept the old to make it more clear
+            //return canExecute == null ? true : canExecute((T)parameter);
+            return canExecute == null || canExecute((T)parameter);
         }
 
-        /// <summary>
-        /// Execute the command's Action
-        /// </summary>
+        ///<summary>
+        ///Occurs when changes occur that affect whether or not the command should execute.
+        ///</summary>
+        public event EventHandler CanExecuteChanged
+        {
+            //Apparently can fire more often than needed, so it can cause problems when using CommandManager.RequerySuggested if your canExecute functions are complex
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        ///<summary>
+        ///Defines the method to be called when the command is invoked.
+        ///</summary>
+        ///<param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
         public void Execute(object parameter)
         {
-            action();
+            execute((T)parameter);
         }
 
         #endregion
