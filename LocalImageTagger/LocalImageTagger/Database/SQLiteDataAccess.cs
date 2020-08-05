e552200998ac69a48f781a726bd2cc18200d195d
Colin Brooks <brookstco@gmail.com>
2020-08-05 16:40:15 -0400

    Database access transactions
    SQLite uses transactions, so commands need to be bundled or there are massive efficiency losses. Some bugs with the parameters currently

--- LocalImageTagger/LocalImageTagger/Database/SQLiteDataAccess.cs
+++ LocalImageTagger/LocalImageTagger/Database/SQLiteDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using LocalImageTagger.Files;
+using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -28,25 +29,51 @@ namespace LocalImageTagger.Database
         public static List<FileItem> LoadFiles()
         {
             //The using will ensure that the DB is closed, even on a crash
-            using (IDbConnection db = new SQLiteConnection(LoadConnectionString()))
+            using (IDbConnection cn = new SQLiteConnection(LoadConnectionString()))
             {
                 //Returns an enumerabe
-                var output = db.Query<FileItem>("select * from Files", new DynamicParameters());
+                var output = cn.Query<FileItem>("select * from Files", new DynamicParameters());
                 return output.ToList();
             }
         }
 
         /// <summary>
-        /// Adds a new File to the DB.
+        /// Adds <see cref="NewFile"/>s to the database. The files are passed in in any IEnumerable, and can have any number.
+        /// Due to the nature of SQLITE, passing them all in and using 1 transaction is vastly more efficient that calling this func many times.
         /// </summary>
-        /// <param name="Uri">The full path to the file.</param>
-        public static void AddFiles(string Uri)
+        /// <param name="files">List of 1 or more <see cref="NewFile"/> files.</param>
+        /// <returns>The <see cref="int"/> amount of modified records.</returns>
+        public static int AddNewFiles(IEnumerable<NewFile> files)
         {
-            using (IDbConnection db = new SQLiteConnection(LoadConnectionString()))
-            {
-                db.Execute("insert into Files (URI) values (Uri) ", new { Uri });
-            }
+            //Used code from https://www.codeproject.com/Articles/853842/Csharp-Avoiding-Performance-Issues-with-Inserts-in to ensure that SQLITE will insert efficiently
 
+            var results = new List<int>();
+            string sqlInsertFile = @"INSERT INTO Files (FullPath) VALUES (@Path);";
+
+            using (var cn = new SQLiteConnection(LoadConnectionString()))
+            {
+                //OPening shoiuld be implicit in the using
+                //cn.Open();
+                //Transatctions always happen in SQLite and doing a transaction per insert is terribly slow
+                using (var transaction = cn.BeginTransaction())
+                {
+                    using (var cmd = cn.CreateCommand())
+                    {
+                        cmd.CommandText = sqlInsertFile;
+                        cmd.Parameters.AddWithValue("@Path", SqliteType.Text);
+
+                        //Insert each file in the list
+                        foreach (var file in files)
+                        {
+                            cmd.Parameters["@Path"] = file.FullPath;
+                            results.Add(cmd.ExecuteNonQuery());
+                        }
+                    }
+                    //Finishes and commits the transaction
+                    transaction.Commit();
+                }
+            }
+            return results.Sum();
         }
 
     }
